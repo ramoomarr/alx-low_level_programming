6e2b8062bf0cc003bc139a0dc06ffe9e32b28728
ramoomarr <oomarahmed170@gmail.com>
2023-08-10 09:40:03 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
  * Return: 1 (error).
 */
 
-int main(void)
-{
-  char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  
+int main(void) {
+  char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
   int len = sizeof(quote) - 1;
   int fd = STDERR_FILENO;
+
   write(fd, quote, len);
   write(fd, "\n", 1);
+
   return 1;
 }
