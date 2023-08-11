085e94504621d92899583bd72cda052faf92b233
ramoomarr <oomarahmed170@gmail.com>
2023-08-11 07:21:54 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+/*
+ * Description: This program prints the alphabet in lowercase
+ * Author: Bard
+ * Date: 2023-08-11
+ */
+
+#include <stdio.h>
+
+/**
+ * main - This is the main function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+
+	ch = "a";
+
+
+	while (ch < = "z")
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
